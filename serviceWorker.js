5c246fd5cfd033dd043e1805662a2b110bc7f625
Fahrezi <luthfi0071@gmail.com>
2024-01-28 00:29:14 +0700

    Registration service worker in every page

--- serviceWorker.js
+++ serviceWorker.js
@@ -15,7 +15,6 @@ self.addEventListener('install', (event) => {
 
 //Listen for request
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
   event.respondWith(
     caches.match(event.request)
       .then(() => {
